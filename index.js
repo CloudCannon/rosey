4748c85b28d6da0046f055e487ee35761f17217c
Jonatah <benattijs@gmail.com>
2020-03-10 14:09:52 +1300

    Add Test Coverage for cli.js

--- index.js
+++ index.js
@@ -4,24 +4,10 @@ const meow = require('meow');
 const helpString = `
 Usage: i18n <command> <flags>
 Flags:
-    -s | --source   The source folder to clone. Defaults to dist/site.
-    -d | --dest     The destination folder to clone the files to. Defaults to dist/prod
-    -b | --baseurl  The filename to prepend to the files in the source.
-    -p | --port     The portnumber to serve the cloned site on.
-    --split         The number of partitions to divide files into
-    --partition     The partition number to process
 
 Commands:
     --Command--                                                     --Reqd flags--
-    build           Compiles HTML and CSS to be run at a baseurl.      --baseurl
-    clean           Removes all files from the dest folder.            --dest
-    clone-assets    Clones non CSS and HTML files from src to dest.    --baseurl
-    rewrite-css     Clones CSS files from src to dest and rewrites     
-                    urls to include baseurl.                           --baseurl
-    rewrite-html    Clones HTML files from src to dest and rewrites
-                    attributes to include baseurl.                     --baseurl
-    serve           Runs a local webserver on the dest folder.
-    watch           Watches the src folder and triggers builds.
+    clean           Removes all files from the dest folder.            
 `
 
 
--- test/cli.js
+++ test/cli.js
@@ -15,3 +15,76 @@ describe("checkRequiredFlags()", function() {
         })
     }) 
 })
+
+describe("setOptions()", function() {
+    // context ("Receives flags from cli", function() {
+    //     let flags = {"baseurl": "testurl", "port": 9898, "dest": "testdest", "source": "testsource", "overwrite": true}
+    //     it ("should return with the correct flags set", function() {
+    //         let options = cli.setOptions( flags );
+    //         expect(options.dist.baseurl).to.equal("testurl");
+    //         expect(options.serve.port).to.equal(9898);
+    //         expect(options.dist.dest).to.equal("testdest");
+    //         expect(options.dist.src).to.equal("testsource");
+
+    //         expect(options.flags.split).to.equal(1);
+    //         expect(options.flags.partition).to.equal(1);
+    //     })
+    // })
+    context ("Receives default options", function() {
+        let flags = {}
+        // let opts = {
+        //     flags:flags,
+        //     help:"helpMessage"
+        // };
+        it ("should return with the correct default settings", function() {
+
+            let options = cli.setOptions( {flags, help:"helpMessage"} );
+            expect(options.cwd).to.equal(process.cwd());
+            expect(options.help).to.equal("helpMessage");
+        })
+    })
+})
+
+describe("run()", function() {
+    // before(function(){
+    //     fs.mkdirSync("test/forTesting");
+    //     fs.writeFileSync("test/forTesting/image.jpg", "image");
+    // })
+    context ("User enters invalid command", function() {
+        let inputs = {flags: {}, input: ["invalidcommand"]}
+        it ("Should exit with code 1", async function() {
+            let exitCode = await cli.run( inputs );
+            expect(exitCode).to.equal(1);
+        })
+    })
+
+    
+    context ("User enters valid command", function() {
+        
+        let inputs = {flags: {}, input: ["help"]}
+        it ("Should exit with code (0)", async function() {
+            let exitCode = await cli.run( inputs );
+            expect(exitCode).to.equal(0);
+        })
+    })
+    
+    // context ("Command runs but fails", function() {
+    //     let inputs = {flags: {"baseurl": "test", "source": "test/invalidplace", "dest": "test/forTesting"}, input: ["clone-assets"]}
+    //     it("Should exit with code (1)", async function() {
+    //         let exitCode = await cli.run( inputs );
+    //         expect(exitCode).to.equal(1);
+    //     })
+    // })
+
+    // context ("User misses required flag", function() {
+    //     let inputs = {flags: {}, input: ["build"]}
+    //     it ("Should exit with code 1", async function() {
+    //         let exitCode = await cli.run( inputs );
+    //         expect(exitCode).to.equal(1);
+    //     })
+    // })
+
+    // after(function(){
+    //     fs.removeSync("test/forTesting")
+    // })
+})
