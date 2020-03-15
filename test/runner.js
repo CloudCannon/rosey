49de6dd80731ce9cfcbecc0fc7706ee8a0655c09
Jonatah <benattijs@gmail.com>
2020-03-16 10:51:24 +1300

    Implemented _clonePrelocalisedHtmlPages
    Fixed messaging. Fixed v2 translation. Added aditional tests.

--- test/runner.js
+++ test/runner.js
@@ -93,6 +93,7 @@ describe("build", function() {
                 <head>
                     <meta charset="utf-8">
                     <meta name="viewport" content="width=device-width, initial-scale=1">
+                    <meta http-equiv="refresh" content="0;URL='/subfolder/'" /> 
                 </head>
                 <body>
                     <header>
@@ -111,7 +112,7 @@ describe("build", function() {
                     </header>
                     <section class="hero diagonal">
                         <div class="container">
-                            <h2 data-i18n="homepage-title" class="editable">We build nice websites</h2>
+                            <h2 data-i18n="homepage-title" data-i18n-attrs="descript" descript="random description to be translated" class="editable">We build nice websites</h2>
                             <p data-i18n="homepage-company-description" class="subtext editable">This is the <strong>Urban</strong> template from <a href="https://cloudcannon.com/">CloudCannon</a>. Urban is a strong foundation for the web presence of your agency.</p>
                             <p><a data-i18n="contact-us" class="button alt" href="/contact/">Contact Us</a></p>
                         </div>
@@ -150,21 +151,17 @@ describe("build", function() {
                     <section class="diagonal patterned">
                         <div class="container halves">
                             <div>
-                                <h3 data-i18n="some-of-our-work" class="editable">Some of our work</h3>
+                                <h3 data-i18n="some-of-our-work" data-i18n-attrs="alt" class="editable">Some of our work</h3>
                                 <p data-i18n="portfolio-description" class="editable">We take pride in our previous work and our happy customers. We cater to any sector to boost business and increase exposure.</p>
                                 <p data-i18n="view-portfolio" class="editable"><a href="/portfolio/">View Full Portfolio &rarr;</a></p>
+                                <p data-i18n class="editable"><a href="/portfolio/">Missing key?</a></p>
                             </div>
                             <div>
                                 <ul class="image-grid">
-                                    
                                         <li><a href="/clients/cause/"><img src="/images/clients/cause.jpg"></a></li>
-                                    
                                         <li><a href="/clients/edition/"><img src="/images/clients/edition.png"></a></li>
-                                    
                                         <li><a href="/clients/frisco/"><img src="/images/clients/frisco.jpg"></a></li>
-                                    
                                         <li><a href="/clients/hydra/"><img src="/images/clients/hydra.png"></a></li>
-                                    
                                 </ul>
                             </div>
                         </div>
@@ -177,7 +174,46 @@ describe("build", function() {
                     </script>
                 </body>
             </html>
-            `;;
+            `;
+            
+        let preLocalized = `
+        <!doctype html>
+            <html lang="en">
+                <head>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1">
+                </head>
+                <body>
+                    <header>
+                        <div class="container">
+                            <h1 class="company-name"><a href="/"><img src="/images/logo.svg" alt="Urban" width="150"></a></h1>
+                            <nav>
+                                <a class="nav-toggle" id="open-nav" href="#">&#9776;</a>
+                                <ul>
+                                    <li><a href="/pt-PT/portfolio/" class="" >Clientes</a></li>
+                                    <li><a href="/pt-BR/blog/" class="" >Blog</a></li>
+                                    <li><a href="/about/" class="" >Sobre Nos</a></li>
+                                    <li><a href="/contact/" class="" >Contatos</a></li>
+                                </ul>
+                            </nav>=
+                        </div>
+                    </header>
+                    <section class="hero diagonal">
+                        <div class="container">
+                            <h2 class="editable">We build nice websites</h2>
+                            <p class="subtext editable">This is the <strong>Urban</strong> template from <a href="https://cloudcannon.com/">CloudCannon</a>. Urban is a strong foundation for the web presence of your agency.</p>
+                            <p><a class="button alt" href="/contact/">Contact Us</a></p>
+                        </div>
+                    </section>
+                    <script>
+                        document.getElementById("open-nav").onclick = function () {
+                            document.body.classList.toggle("nav-open");
+                            return false;
+                        };
+                    </script>
+                </body>
+            </html>
+            `;
 
 
         //Creat Source Files
@@ -185,26 +221,45 @@ describe("build", function() {
         fs.mkdirSync(options.i18n.source+"/assets");
         fs.mkdirSync(options.i18n.source+"/css");
         fs.mkdirSync(options.i18n.source+"/html");
+        fs.mkdirSync(options.i18n.source+"/pt-BR/");
         fs.writeFileSync(options.i18n.source+"/image.jpg", "image");
         fs.writeFileSync(options.i18n.source+"/assets/image2.jpg", "image");
         fs.writeFileSync(options.i18n.source+"/style.css", "css");
         fs.writeFileSync(options.i18n.source+"/css/style2.css", "css");
         fs.writeFileSync(options.i18n.source+"/index.html", html);
         fs.writeFileSync(options.i18n.source+"/html/index2.html", html2);
+        fs.writeFileSync(options.i18n.source+"/pt-BR/preLocalized.html", preLocalized);
 
         //Create Locales
         fs.mkdirSync(options.i18n.generated_locale_dest);
         fs.mkdirSync(options.i18n.locale_source);
         localeBR = {
-            "bottom-title": "Última tecnologia, ótimo desempenho",
+            "homepage-title": "Criamos websites para você",
+            "homepage-title.descript": "Descrição aleatória a ser traduzida",
             "contact-us": "Entre em contato",
             "some-of-our-work": "Um pouco do nosso trabalho"
         };
         localePT = {
-            "bottom-title": "POISH, Última tecnologia, ótimo desempenho"
+            "homepage-title": "POISH, Criamos websites para você"
+        };
+        localeFR = {
+            "homepage-title": {
+                original: "We build nice website",
+                value: "Nous construisons de beaux sites Web"
+            },
+            "homepage-title.descript": {
+                original: "random description to be translated",
+                value: "Nous construisons de beaux sites Web"
+            },
+            "contact-us": {
+                original: "Contact Us",
+                value: "Nous contacter"
+            }
+
         };
         fs.writeJsonSync(options.i18n.locale_source+"/pt-BR.json",localeBR);
         fs.writeJsonSync(options.i18n.locale_source+"/pt-PT.json",localePT);
+        fs.writeJsonSync(options.i18n.locale_source+"/fr.json",localeFR);
         fs.writeFileSync(options.i18n.locale_source+"/es.json","Wrong JSON");
         fs.writeFileSync(options.i18n.locale_source+"/invalid.INVALID","Wrong JSON");
     })
@@ -224,12 +279,15 @@ describe("build", function() {
         it("should have a pt-PT folder on the dest", async function(){
             expect(fs.existsSync(options.i18n.full_dest+"/pt-PT/")).to.equal(true);
         })
+        it("should have a fr folder on the dest", async function(){
+            expect(fs.existsSync(options.i18n.full_dest+"/fr/")).to.equal(true);
+        })
         
-        it("should have a en folder on the dest", async function(){
+        it("should have an en folder on the dest", async function(){
             expect(fs.existsSync(options.i18n.full_dest+"/en/")).to.equal(true);
         })
         
-        it("should NOT have a es folder on the dest", async function(){
+        it("should NOT have an es folder on the dest", async function(){
             expect(fs.existsSync(options.i18n.full_dest+"/es/")).to.equal(false);
         })
     })
